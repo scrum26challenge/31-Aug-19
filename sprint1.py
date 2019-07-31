ffe96af9bf938b540a7cc75e581e5df3bd2db4d7
Ssawalha <49880120+Ssawalha@users.noreply.github.com>
2019-07-31 19:59:37 -0400

    added print filename

--- sprint1.py
+++ sprint1.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 filename = sys.argv[1]
 
+print(filename)
+
 data = pd.read_csv(filename)
 print("Number of Rows:" + len(data.index))
 print("Number of Columns:" + len(data.columns))
