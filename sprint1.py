8303e4dc047a9bbe0c4dff36ed4cc6a0143fc7b5
Ssawalha <49880120+Ssawalha@users.noreply.github.com>
2019-07-31 19:58:58 -0400

    fixed filename reader from terminal

--- sprint1.py
+++ sprint1.py
@@ -1,10 +1,8 @@
 import sys
-filename = sys.argv[1]
-
-readf = open(filename, "r")
-
 import pandas as pd
 
-data = pd.read_csv("replacewithfilename")
+filename = sys.argv[1]
+
+data = pd.read_csv(filename)
 print("Number of Rows:" + len(data.index))
 print("Number of Columns:" + len(data.columns))
