c8ccc0add5ff01c03246ff5ffe391d6597184b70
sejalgupta <sgupta.4902@gmail.com>
2019-07-31 20:00:33 -0400

    Update sprint1.py

--- sprint1.py
+++ sprint1.py
@@ -6,5 +6,5 @@ filename = sys.argv[1]
 print(filename)
 
 data = pd.read_csv(filename)
-print("Number of Rows:" + len(data.index))
-print("Number of Columns:" + len(data.columns))
+print("Number of Rows:" + str(len(data.index)))
+print("Number of Columns:" + str(len(data.columns)))
